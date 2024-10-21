61ab6a424da4ea2c6251a44ec20f505cee8e1d0b
soundhar11 <soundharj27@gmail.com>
2024-10-21 10:59:20 +0530

    Emp;oyee.cpp updated

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+class Employee{
+    public:
+        string employeeId;
+        string employeeName;
+        int age;
+        int mobileNumber;
+        string department;
+        double salary;
+
+        Employee(){
+            
+        }
+
+
+};
+int main(){
+
+}
